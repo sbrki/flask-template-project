9cd3dd2430941007044c74f8629c2dffe18b6cda
Stjepan <whiteShtef@users.noreply.github.com>
2017-07-17 14:22:09 +0200

    Update tools
    Add hash_file method

--- app/tools/__init__.py
+++ app/tools/__init__.py
@@ -5,6 +5,12 @@ def hash(string):
 	"""Returns a SHA224 hash of a string, in a hex-dump string format."""
 	return hashlib.sha224(string.encode()).hexdigest()
 
+def hash_file(file_object):
+	"""Returns a SHA224 hash of a file, in a hex-dump string format."""
+	file_data = file_object.read()
+	file_object.seek(0,0)
+	return hashlib.sha224(file_data).hexdigest()
+
 def sanitize(string):
 	"""
 	Sanitizes a string from HTML "dangerous" stuff.
